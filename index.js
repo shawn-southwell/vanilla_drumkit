21f25146424e9108357239072c05b003b49676b6
Shawn Southwell <shawn.southwel@gmail.com>
2016-12-11 16:22:45 -0800

    Add/remove class on click

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*
 65, 83, 68, 70, 74, 75, 76, 186
 */
+
 const soundz = {
   65: 'sounds/boom.wav',
   83: 'sounds/clap.wav',
@@ -17,9 +18,8 @@ document.addEventListener('keydown', (e) => {
   const file = soundz[String(e.which)];
   const dataAttr = file.slice(file.indexOf('/') + 1,file.indexOf('.'));
   const el = document.querySelectorAll(`[data-sound=${dataAttr}]`);
-  console.log(el);
   el[0].classList.add('playing');
   const audio = new Audio(file);
   audio.play();
-  
-})
+  setTimeout(()=>{el[0].classList.remove('playing')}, 50);
+});
