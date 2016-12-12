01f05751b4727e0d26d2416897852c7830ddfef2
Shawn Southwell <shawn.southwel@gmail.com>
2016-12-11 16:10:36 -0800

    Functional drum kit, need to add remove classes based on keydown

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+65, 83, 68, 70, 74, 75, 76, 186
+*/
+const soundz = {
+  65: 'sounds/boom.wav',
+  83: 'sounds/clap.wav',
+  68: 'sounds/hihat.wav',
+  70: 'sounds/kick.wav',
+  74: 'sounds/openhat.wav',
+  75: 'sounds/ride.wav',
+  76: 'sounds/snare.wav',
+  186: 'sounds/tink.wav',
+  32: 'sounds/tom.wav'
+}
+
+document.addEventListener('keydown', (e) => {
+  const file = soundz[String(e.which)];
+  const dataAttr = file.slice(file.indexOf('/') + 1,file.indexOf('.'));
+  const el = document.querySelectorAll(`[data-sound=${dataAttr}]`);
+  console.log(el);
+  el[0].classList.add('playing');
+  const audio = new Audio(file);
+  audio.play();
+  
+})
